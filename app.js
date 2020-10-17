8418f307eeb381233fbff981794775dc9e39c0ce
kino <kinonote13@gmail.com>
2020-10-17 18:26:07 +0900

    正解数を表示する

--- app.js
+++ app.js
@@ -32,15 +32,8 @@ const quiz = [
 
 const quizLength = quiz.length;
 let quizIndex = 0;
+let score = 0;
 
-// const question = 'ドラえもんが２０世紀に来て、初めて食べた食べ物は何？';
-// const answers = [
-//   '栗まんじゅう',
-//   'お餅',
-//   'どら焼き',
-//   '焼き芋'
-// ];
-// const correct = 'お餅';
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 //クイズの問題文、選択肢を定義
@@ -60,6 +53,7 @@ setupQuiz();
 const clickHandler = (e) => {
   if (quiz[quizIndex].correct === e.target.textContent) {
     window.alert('正解！');
+    score++;
   } else {
     window.alert('不正解！');
   }
@@ -69,7 +63,7 @@ const clickHandler = (e) => {
     setupQuiz();
   } else {
     //問題数がもうなければこちらを実行
-    window.alert('終了！');
+    window.alert('終了！あなたの正解数は'+score+'/'+quizLength+'です!');
   }
 
 };
