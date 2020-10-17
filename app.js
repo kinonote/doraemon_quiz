10c11afc9cf5b01235332cab83bf2ebefd959c88
kino <kinonote13@gmail.com>
2020-10-17 18:09:40 +0900

    問題数を増やしたから、配列にした

--- app.js
+++ app.js
@@ -1,21 +1,56 @@
-const question = 'ドラえもんが２０世紀に来て、初めて食べた食べ物は何？';
-const answers = [
-  '栗まんじゅう',
-  'お餅',
-  'どら焼き',
-  '焼き芋'
+
+const quiz = [
+  {
+    question: 'ドラえもんが２０世紀に来て、初めて食べた食べ物は何？',
+    answers: [
+      '栗まんじゅう',
+      'お餅',
+      'どら焼き',
+      '焼き芋'
+    ],
+    correct:'お餅'
+  },  {
+    question: 'のび太が初めて使ったドラえもんの道具は何？',
+    answers: [
+      'どこでもドア',
+      'タケコプター',
+      'タイムマシン',
+      'ヘリトンボ'
+    ],
+    correct:'ヘリトンボ'
+  },  {
+    question: 'ドラえもんの座高は？',
+    answers: [
+      '129.3cm',
+      '90cm',
+      '100cm',
+      '110cm'
+    ],
+    correct:'100cm'
+  }
 ];
-const correct = 'お餅';
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+
+// const question = 'ドラえもんが２０世紀に来て、初めて食べた食べ物は何？';
+// const answers = [
+//   '栗まんじゅう',
+//   'お餅',
+//   'どら焼き',
+//   '焼き芋'
+// ];
+// const correct = 'お餅';
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 //クイズの問題文、選択肢を定義
 const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
 
   let buttonIndex = 0;
   
   while (buttonIndex < buttonLength) {
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
@@ -23,11 +58,20 @@ const setupQuiz = () => {
 setupQuiz();
 
 const clickHandler = (e) => {
-  if (correct === e.target.textContent) {
+  if (quiz[quizIndex].correct === e.target.textContent) {
     window.alert('正解！');
   } else {
     window.alert('不正解！');
   }
+  quizIndex++;
+  if (quizIndex<quizLength) { 
+    //問題数がまだあればこちらを実行
+    setupQuiz();
+  } else {
+    //問題数がもうなければこちらを実行
+    window.alert('終了！');
+  }
+
 };
 //ボタンをクリックしたら正誤判定
 let handleIndex = 0;
@@ -39,18 +83,3 @@ while (handleIndex < buttonLength) {
   handleIndex++;
 }
 
-// $button[0].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
-
-// $button [1].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
-
-// $button [2].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
-
-// $button [3].addEventListener('click', (e) => {
-//   clickHandler(e);
-// });
